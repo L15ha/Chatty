e675fcd78df4867a0fe3a325c4d093373dc9d4ac
L15ha <bossss992@gmail.com>
2025-04-25 10:03:48 +0700

    Fix endpoint and key

--- Chatty.py
+++ Chatty.py
@@ -15,7 +15,7 @@ load_dotenv()
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_KEY"),
     api_version="2024-12-01-preview",
-    azure_endpoint="https://mju64-m9usb3dg-eastus2.cognitiveservices.azure.com/"
+    azure_endpoint="https://sirinat.openai.azure.com/"
 )
 
 DEPLOYMENT_NAME = "Chatty"
